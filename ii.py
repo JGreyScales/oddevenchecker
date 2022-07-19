0fddb75225a7400222be4f6ba01f1804f38cb2d1
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2022-07-19 19:29:07 -0400

    alignment fix

--- ii.py
+++ ii.py
@@ -9,7 +9,7 @@ for x in range(1, (10**5) + 1):
     y = 'odd'
     if x % 2 == 0:
         y = 'even'
-     file.write(f"if x == '{x}':\n")
-      file.write(f"    print('{y}')\n")
+    file.write(f"if x == '{x}':\n")
+    file.write(f"    print('{y}')\n")
 
 file.close()
